2f19f06e20cee6be7676310c9f7ce180a12f54cd
tomischdcd <t0m15ch@macbook-pro-de-tomischdcd.home>
2025-05-25 13:10:40 +0200

    chore: add password hash generation utility
    - Temporary tool for generating BCrypt hashes
    - Used for creating test user accounts
    - Can be removed after workshop completion

---
+++ temp-hash-generator/Program.cs
@@ -0,0 +1,17 @@
+﻿using BCrypt.Net;
+
+Console.WriteLine("Génération des hashes pour Password123!");
+Console.WriteLine();
+
+var password = "Password123!";
+
+for (int i = 0; i < 3; i++)
+{
+    var hash = BCrypt.Net.BCrypt.HashPassword(password);
+    Console.WriteLine($"Hash {i + 1}: {hash}");
+}
+
+// Vérification
+var testHash = BCrypt.Net.BCrypt.HashPassword(password);
+var isValid = BCrypt.Net.BCrypt.Verify(password, testHash);
+Console.WriteLine($"\nVérification: {isValid}");
